f1c37d4f320c35db628e97966cb58ff51c91dbb7
Samjay <barnabassampana89@gmail.com>
2022-07-06 22:39:41 +0000

    Complete Version of Student Website

--- public/js/script.js
+++ public/js/script.js
@@ -18,18 +18,43 @@ let profilebtn = document.querySelector('#profilebtn');
             console.log('profilebtn')
             if(menulist.style.display === 'none'){
                 menulist.style.display = 'inline'
+                console.log('show')
             }else{
                 menulist.style.display = 'none'
+                console.log('hide')
             }
         })
         
         // CLOSES MENULIST WHEN YOU CLICK ANYWHERE OUTSIDE
         window.onclick = (e)=>{
+            console.log('id',e.target.id,e.target)
             if(e.target.id !== 'profileimg'){
                 menulist.style.display = 'none'
             }
         }
 
+        // INDEX.HTML CODE FOR MENU DROPDOWN
+        // let menu = document.querySelector('#menu');
+        // let menuItems = document.querySelector('#menuItems')
+
+        // // menuItems.style.display = 'none'
+        // menu.addEventListener('click', ()=>{
+        //     console.log('menu')
+        //     if(menuItems.style.display === 'none'){
+        //         menuItems.style.display = 'inline'
+        //         console.log('1')
+        //     }else{
+        //         menuItems.style.display = 'none'
+        //         console.log('2')
+        //     }
+        // })
+        
+        // // CLOSES MENULIST WHEN YOU CLICK ANYWHERE OUTSIDE
+        // window.onclick = (e)=>{
+        //     if(e.target.id !== 'menu'){
+        //         menuItems.style.display = 'none'
+        //     }
+        // }
 
 
 
